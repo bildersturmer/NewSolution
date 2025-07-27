60a03b7b6256fb04d2ecb5f18d1dd58b9d32f177
bilder sturmer <smurray@belfastmet.ac.uk>
2025-07-27 18:32:23 +0000

    Needed to annotate the 2 additional test methods with [TestMethod]

--- TestApp/UnitTest1.cs
+++ TestApp/UnitTest1.cs
@@ -11,12 +11,14 @@ public class UnitTest1
     {
         Assert.AreEqual(AppClass.testName, "App Test");
     }
-
+    
+    [TestMethod]
     public void TestMethod2()
     {
-        Assert.AreEqual(AppClass.testName, "App Test");
+        Assert.AreNotEqual(AppClass.testName, "Not App Test");
     }
 
+    [TestMethod]
     public void TestMethod3()
     {
         Assert.AreEqual(AppClass.testName,"App Test" );
